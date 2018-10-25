ac91bb9c393ff329e3562088c294b5f989f9cdfb
Bill Kwaku Ansah Inkoom <billkainkoom@Bills-MacBook-Pro.local>
2018-10-25 09:54:45 +0000

    First meeting @ Wednesday 24th October 2018 - We covered brief history of android as well as some basic design principles. Did a little work on UI as well as a Kotlin introduction

---
+++ app/src/main/java/com/hubtel/galaconf2018/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.hubtel.galaconf2018
+
+import android.content.Context
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    lateinit var context: Context
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        context = this
+        var myFab = findViewById<FloatingActionButton>(R.id.fab)
+
+
+        myFab.setOnClickListener {
+            Toast.makeText(context, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
+
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
+
+
+
+
+
+
+
